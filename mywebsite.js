249d4ee53220649c7f7bb5114c8325c2673c3f77
Cam Wilson <camwils0210@outlook.com>
2024-03-14 02:47:04 -0400

    Add files via upload

---
+++ mywebsite.js
@@ -0,0 +1,19 @@
+var client_id = '9281f899624f47be8d2f0d1842a635cc';
+var client_secret = 'b63145d3be3142218ebedfed790844bf';
+
+var authOptions = {
+  url: 'https://accounts.spotify.com/api/token',
+  headers: {
+    'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+  },
+  form: {
+    grant_type: 'client_credentials'
+  },
+  json: true
+};
+
+request.post(authOptions, function(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    var token = body.access_token;
+  }
+});
